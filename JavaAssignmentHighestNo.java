d5efd45b095d96babe6cd854ff11695762e34b73
TamoghnaChattopadhyay <tchitamo3@gmail.com>
2017-01-08 12:12:18 +0530

    JavaHighestNo

---
+++ JavaAssignmentHighestNo.java
@@ -0,0 +1,23 @@
+public class JavaHighestNo {
+
+	public static void main(String[] args) {
+		
+	int a=10;
+	int b=30;
+	int c= 20;
+		if(a>=b && a>=c) 
+		{
+		System.out.println("Largest number: "+a);
+		}
+		if(b>=a && b>=c) 
+		{
+		System.out.println("Largest number: "+b);
+		}
+		if(c>=a && c>=b) 
+		{
+		System.out.println("Largest number: "+c);
+
+	}
+
+}
+}
